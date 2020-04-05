9befc50a351fe22d46591e656ea97773f5752c6a
elazar cohen <escohen115@gmail.com>
2020-04-05 22:02:23 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,5 @@
 def display_rainbow (colors)
   puts "R: #{colors[0]}"
-  puts "O: #{colors[0]}"
-  puts "Y: #{colors[0]}"
-  puts "R: #{colors[0]}"
+  puts "O: #{colors[1]}"
+  puts "Y: #{colors[2]}"
+  puts "G: #{colors[3]}"
